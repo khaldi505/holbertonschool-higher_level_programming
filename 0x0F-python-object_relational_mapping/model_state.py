6e9c36dc7113f936a1ba3c70159816e75b683a1d
khaldi505 <habetlbrika69@gmail.com>
2020-08-09 15:26:58 +0000

    model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' i'll update this late '''
+import sys
+from model_state import Base, state
+import sqlalchemy
+if __name__ == "__main__":
+	engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format("root", "root", "my_db"), pool_pre_ping=True)
+	Base.metadata.create_all(engine)
