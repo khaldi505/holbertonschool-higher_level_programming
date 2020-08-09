05752162161697d94512187eb190cdf258a75551
khaldi505 <habetlbrika69@gmail.com>
2020-08-09 13:21:17 +0000

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
-'''lists all states from the database hbtn_0e_0_usa'''
-db = MySQLdb.connect(host="localhost", user=sys.argv[
-                     1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-db.close()
+''' script that lists all states from the database '''
+if __name__ == "__main__":
+
+    import MySQLdb
+
+    from sys import argv
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    my_database = MySQLdb.connect(host="localhost", user=username,
+                                  passwd=password, port=3306, db=database)
+    mycrusor = my_database.cursor()
+    mycrusor.execute("SELECT * FROM states ORDER BY id ASC")
+    for state in mycrusor:
+        print(state)
