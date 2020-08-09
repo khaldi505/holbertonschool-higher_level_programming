aabc3c351459953c3d97107ac6f174e232ab6e2d
khaldi505 <53900877+khaldi505@users.noreply.github.com>
2020-08-09 15:25:29 +0100

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""sql injection f u hacker man"""
+
+"""script that takes in an argument and displays all values in the states"""
+
 if __name__ == "__main__":
-  import sys
-  import MySQLdb
-  db = MySQLdb.connect(host="localhost", user=sys.argv[
-                       1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
-  cur = db.cursor()
-  cur.execute("SELECT * FROM states ORDER BY id ASC")
-  query_rows = cur.fetchall()
-  for row in query_rows:
-      if (row[1] == sys.argv[4]):
-          print("{}".format(row))
-  cur.close()
-  db.close()
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host="localhost", user=sys.argv[
+                         1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if (row[1] == sys.argv[4]):
+            print("{}".format(row))
+    cur.close()
+    db.close()
