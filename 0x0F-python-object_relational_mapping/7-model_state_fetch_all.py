2954b20026621cebf8f727a5f0ad9e77cb8c3755
khaldi505 <53900877+khaldi505@users.noreply.github.com>
2020-08-11 12:51:18 +0100

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
- #!/usr/bin/python3
- """ script that lists all State objects
+#!/usr/bin/python3
+""" script that lists all State objects
  from the database hbtn_0e_6_usa """
 from sqlalchemy import (create_engine)
 from model_state import Base, State
