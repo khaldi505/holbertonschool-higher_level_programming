33fdfe8676e1ee2ef6483786534157c9820ab51e
khaldi505 <habetlbrika69@gmail.com>
2020-08-24 15:51:28 +0000

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+
 """ sends a request to the URL and displays the value
     of the X-Request-Id variable found in the header of the response.
 """
 
 import urllib.request
 from sys import argv
-with urllib.request.urlopen(argv[1]) as response:
-    req = response.info()
-    print("{}".format(req["X-Request-Id"]))
+
+
+def go_go_power_rangers():
+
+    with urllib.request.urlopen(argv[1]) as response:
+        req = response.info()
+        print("{}".format(req["X-Request-Id"]))
+
+if __name__ == "__main__":
+    go_go_power_rangers()
