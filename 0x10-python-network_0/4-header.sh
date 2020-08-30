8e338e8886d7724274fe119635d1cebdc2f19a94
khaldi505 <habetlbrika69@gmail.com>
2020-08-30 10:04:12 +0000

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL as an argument, sends a GET request
-curl -sL -X GET -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s "$1"-H "X-HolbertonSchool-User-Id: 98" 
