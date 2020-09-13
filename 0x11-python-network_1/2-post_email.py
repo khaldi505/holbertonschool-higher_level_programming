1c0f9d1851e0dd32155a679e459ba18355a22d33
khaldi505 <habetlbrika69@gmail.com>
2020-09-13 13:01:33 +0100

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# post an email on a website
+from urllib import request, parse
+from sys import argv as argv
+def go_go_power_rangers():
+	email = parse.urlencode({"email": argv[2]})
+	email = email.encode("ascii")
+	url = argv[1]
+	with request.urlopen(url, email) as rep:
+	    info = rep.read()
+	print(info)
+if __name__ == "__main__":
+    go_go_power_rangers()
