eff619b23602cb8f7f11effb788e1c1cf417bd76
khaldi505 <habetlbrika69@gmail.com>
2020-09-13 13:08:03 +0100

    coding style fixed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-# post an email on a website
+"""
+post an email on a website
+"""
 from urllib import request, parse
 from sys import argv as argv
+
+
 def go_go_power_rangers():
-	email = parse.urlencode({"email": argv[2]})
-	email = email.encode("ascii")
-	url = argv[1]
-	with request.urlopen(url, email) as rep:
-	    info = rep.read()
-	print(info.decode("UTF-8"))
+    email = parse.urlencode({"email": argv[2]})
+    email = email.encode("ascii")
+    url = argv[1]
+    with request.urlopen(url, email) as rep:
+        info = rep.read()
+    print(info.decode("UTF-8"))
+
+
 if __name__ == "__main__":
     go_go_power_rangers()
